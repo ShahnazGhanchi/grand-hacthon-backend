a85a6c1fa0f8af31551e93b7402f70375a8474a0
ShahnazGhanchi <shahnazghanchi86@gmail.com>
2026-08-30 16:27:07 +0500

    Fix Vercel deployment

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
-// 1. Auth Routes Import 
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
 
 const app = express();
 
-// Database Connect
 connectDB();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/products", productRoutes);
 app.use("/api/tickets", ticketRoutes);
 
-
-// Routes
 app.get("/", (req, res) => {
   res.send("E-Commerce Backend Working Fresh! 🚀");
 });
-app.use("/api/auth", authRoutes);// ye routes hum cheak kare gy
 
+app.use("/api/auth", authRoutes);
 
-// Server Start
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Vercel
+module.exports = app;
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 8000;
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is flying on port ${PORT}`);
+  });
+}
